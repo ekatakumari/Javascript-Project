59e179d14c8d012e63d3848845af3bf6d12c7160
ekata <ekata1995@gmail.com>
2025-02-20 19:17:59 +0530

    add

--- my-app/src/Component/Toast/Toast.jsx
+++ my-app/src/Component/Toast/Toast.jsx
@@ -1,23 +1,26 @@
 import "./Toast.css";
+import { useState } from "react"; 
 export const Toast = () => {
     const[toastmessage,settoastmessage]=useState("")
+    const[isToastDiv,setToastDiv]=useState(false)
     const handlemessage=(item)=>{
-  console.log("item",item)
+    setToastDiv(true)
+      settoastmessage(item)
     }
   return (
     <div className="toast-container">
-      <div className="toast-message">
+      {isToastDiv && <div className="toast-message">
         <div className="notification-message">
           {" "}
-          <span> hello</span>
+          <span> {toastmessage}</span>
           <button className="toast-cross">x</button>
         </div>
-      </div>
+      </div>}
       <div className="toast-button-container">
-        <button className="suucess-button btn-toast" onClick={handlemessage("success")}>Success Toast</button>
-        <button className="warning-button btn-toast">Warning Toast</button>
-        <button className="info-button btn-toast">Info Toast</button>
-        <button className="error-button btn-toast">Error Toast</button>
+        <button className="suucess-button btn-toast" onClick={()=>handlemessage("success")}>Success Toast</button>
+        <button className="warning-button btn-toast" onClick={()=>handlemessage("Warning")}>Warning Toast</button>
+        <button className="info-button btn-toast" onClick={()=>handlemessage("Info")}>Info Toast</button>
+        <button className="error-button btn-toast" onClick={()=>handlemessage("Error")}>Error Toast</button>
       </div>
     </div>
   );

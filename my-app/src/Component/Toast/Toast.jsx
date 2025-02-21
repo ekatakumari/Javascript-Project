f7b099f83dca1d5b3b949b3bd648352834f7459b
ekata <ekata1995@gmail.com>
2025-02-21 19:05:42 +0530

    add

--- my-app/src/Component/Toast/Toast.jsx
+++ my-app/src/Component/Toast/Toast.jsx
@@ -4,55 +4,56 @@ import { useState } from "react";
 export const Toast = () => {
   const [toastlist, setToastList] = useState([]);
 
-  // Function to remove a specific toast
   const handleCloseToast = (id) => {
     setToastList((prevList) => prevList.filter((toast) => toast.id !== id));
   };
 
-  // Function to add a toast message
   const handlemessage = (item, type) => {
-    const id = new Date()
+    const id = new Date();
     const newToast = [...toastlist, { id, item, type }];
-    console.log(newToast)
     setToastList(newToast);
+    setTimeout(()=>{
+      handleCloseToast(id)
+    },5000)
   };
 
   return (
     <div className="toast-container">
-      {/* Render toast messages */}
-      {toastlist.map(({ id, item, type }) => (
-        <div className={`toast-message ${type}`} key={id}>
-          <div className="notification-message">
+      <div className={`toast-message `}>
+        {toastlist.map(({ id, item, type }) => (
+          <div className={`notification-message ${type}` } key={id}>
             <span>{item}</span>
-            <button className="toast-cross" onClick={() => handleCloseToast(id)}>
+            <button
+              className="toast-cross"
+              onClick={() => handleCloseToast(id)}
+            >
               x
             </button>
           </div>
-        </div>
-      ))}
-
+        ))}
+      </div>
       {/* Toast buttons */}
       <div className="toast-button-container">
         <button
-          className="suucess-button btn-toast"
+          className="success btn-toast"
           onClick={() => handlemessage("Success", "success")}
         >
           Success Toast
         </button>
         <button
-          className="warning-button btn-toast"
+          className="warning btn-toast"
           onClick={() => handlemessage("Warning", "warning")}
         >
           Warning Toast
         </button>
         <button
-          className="info-button btn-toast"
+          className="info btn-toast"
           onClick={() => handlemessage("Info", "info")}
         >
           Info Toast
         </button>
         <button
-          className="error-button btn-toast"
+          className="error btn-toast"
           onClick={() => handlemessage("Error", "error")}
         >
           Error Toast
